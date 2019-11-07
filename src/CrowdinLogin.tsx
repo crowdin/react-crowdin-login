a72c68ae2ef12870608f0b97282f8cde79bdaefa
alexandrtovmach <alexandrtovmach@gmail.com>
2019-11-07 13:04:18 +0200

    feat: Added basic modal window implementation

--- src/CrowdinLogin.tsx
+++ src/CrowdinLogin.tsx
@@ -1,22 +1,51 @@
 import * as React from "react";
+//@ts-ignore
+import Popout from "react-popout";
 
 import { CrowdinLoginProps, CrowdinLoginState } from "../";
 import CrowdinLoginButton from "./CrowdinLoginButton";
 
 export default class CrowdinLoginComponent extends React.Component<
   CrowdinLoginProps,
-  CrowdinLoginState
+  CrowdinLoginState | any
 > {
+  state = {
+    openModal: false
+  };
+
+  handleLoginClick = () => {
+    // window.open("https://accounts.crowdin.com/oauth/authorize", "Popup","toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30")
+    this.setState({
+      openModal: true
+    });
+  };
+
   render() {
     const { buttonTheme, className, children } = this.props;
-    return children ? (
+    const { openModal } = this.state;
+
+    const button = children ? (
       <div onClick={console.log}>{children}</div>
     ) : (
       <CrowdinLoginButton
         buttonTheme={buttonTheme || "light"}
         buttonClassName={className}
-        onClick={console.log}
+        onClick={this.handleLoginClick}
       />
+    );
+
+    return (
+      <>
+        {button}
+        {openModal && (
+          <Popout
+            url="https://accounts.crowdin.com/oauth/authorize"
+            title="Sign in to CrowdIn"
+            onClosing={console.log}
+            options={{ height: "470px" }}
+          />
+        )}
+      </>
     );
   }
 }
